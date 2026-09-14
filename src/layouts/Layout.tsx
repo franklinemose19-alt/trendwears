de6bc65de7400358e99ed831dba63e61869f7598
franklinemose19-alt <franklinemose19@gmail.com>
2026-09-14 10:02:29 +0300

    Update Layout.tsx

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -4,9 +4,9 @@ import Footer from '@/components/Footer'
 
 export default function Layout() {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="flex min-h-screen w-full flex-col overflow-x-hidden">
       <Navbar />
-      <main className="flex-1">
+      <main className="flex-1 overflow-x-hidden">
         <Outlet />
       </main>
       <Footer />
